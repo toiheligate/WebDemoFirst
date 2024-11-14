59e4c4e3b99bff7c3b52b6c5df287c8c27a346c4
Hoang Kim Toi <toiheligate@gmail.com>
2024-11-14 15:42:24 +0700

    Update register page

--- js/register.js
+++ js/register.js
@@ -1,49 +1,95 @@
+// Kiểm tra trạng thái đăng nhập (giả sử bằng localStorage)
+function checkLoginStatus() {
+  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+  document.getElementById("notLoggedIn").classList.toggle("hidden", isLoggedIn);
+  document.getElementById("loggedIn").classList.toggle("hidden", !isLoggedIn);
+  document
+    .getElementById("loggedInNotification")
+    .classList.toggle("hidden", !isLoggedIn);
+  document
+    .getElementById("nav-hidden-button")
+    .classList.toggle("hidden", isLoggedIn);
+}
 
-
-function toggleAnswer(element) {
-    const item = element.parentNode;
-    item.classList.toggle("active");
-  }
-  
-  // Kiểm tra trạng thái đăng nhập (giả sử bằng localStorage)
-  function checkLoginStatus() {
-    const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
-    document.getElementById("notLoggedIn").classList.toggle("hidden", isLoggedIn);
-    document.getElementById("loggedIn").classList.toggle("hidden", !isLoggedIn);
-    document
-      .getElementById("loggedInNotification")
-      .classList.toggle("hidden", !isLoggedIn);
-    document
-      .getElementById("nav-hidden-button")
-      .classList.toggle("hidden", isLoggedIn);
-  }
-  
-  // Hàm đăng nhập (chỉ là ví dụ)
-  function login() {
-    localStorage.setItem("isLoggedIn", "true");
-    checkLoginStatus();
-  }
-  
-  // Hàm đăng xuất
-  function logout() {
-    localStorage.setItem("isLoggedIn", "false");
-    checkLoginStatus();
-  }
-  
-  // Kiểm tra trạng thái khi tải trang
+// Hàm đăng nhập (chỉ là ví dụ)
+function login() {
+  localStorage.setItem("isLoggedIn", "true");
   checkLoginStatus();
-  
-  function togglePassword(classId) {
-    const passwordInput = document.getElementById(`${classId}`);
-    const toggleIcon = document.querySelector(`.toggle-${classId}`);
-    if (passwordInput.type === "password") {
-      passwordInput.type = "text";
-      toggleIcon.classList.remove("fa-eye-slash");
-      toggleIcon.classList.add("fa-eye");
+}
+
+// Hàm đăng xuất
+function logout() {
+  localStorage.setItem("isLoggedIn", "false");
+  checkLoginStatus();
+}
+
+// Kiểm tra trạng thái khi tải trang
+checkLoginStatus();
+
+let registerCurrentStep = 0;
+const steps = document.querySelectorAll(".step");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+const subTitle = document.getElementById("page-subtitle");
+const privacyAndPolicy = document.getElementById("register-privacy-policy-box");
+const emailRegister = document.getElementById("register-email-container");
+const companyComplete = document.getElementById("register-by-company-complete");
+
+function updateStep() {
+  steps.forEach((step, index) => {
+    if (index <= registerCurrentStep) {
+      if (index === registerCurrentStep) {
+        step.classList.remove("actived");
+        step.classList.add("active");
+      } else {
+        step.classList.remove("active");
+        step.classList.add("actived");
+      }
     } else {
-      passwordInput.type = "password";
-      toggleIcon.classList.remove("fa-eye");
-      toggleIcon.classList.add("fa-eye-slash");
+      step.classList.remove("active");
+      step.classList.remove("actived");
     }
+  });
+
+  switch (registerCurrentStep) {
+    case 0:
+      subTitle.textContent = "ご登録前の確認事項";
+      privacyAndPolicy.classList.remove("hidden");
+      emailRegister.classList.add("hidden");
+      companyComplete.classList.add("hidden");
+      break;
+    case 1:
+      subTitle.textContent = "メールアドレス入力";
+      privacyAndPolicy.classList.add("hidden");
+      emailRegister.classList.remove("hidden");
+      companyComplete.classList.add("hidden");
+      break;
+    case 2:
+      subTitle.classList.add("hidden");
+      privacyAndPolicy.classList.add("hidden");
+      emailRegister.classList.add("hidden");
+      companyComplete.classList.remove("hidden");
+      break;
+    default:
+      break;
   }
-  
+
+  prevBtn.disabled = registerCurrentStep === 0;
+  nextBtn.disabled = registerCurrentStep === steps.length - 1;
+}
+
+function nextStep() {
+  if (registerCurrentStep < steps.length - 1) {
+    registerCurrentStep++;
+    updateStep();
+  }
+}
+
+function prevStep() {
+  if (registerCurrentStep > 0) {
+    registerCurrentStep--;
+    updateStep();
+  }
+}
+
+updateStep();
